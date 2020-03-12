e3a3204e28cc69094f1e8c0dfca695f68b498d08
Béla Schaum <schaumb@gmail.com>
2020-03-12 17:59:01 +0100

    rm warning

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 void solver(const Input& in, Output& o) {
 	std::vector<std::vector<bool>> atDesk;
 	std::size_t dev{}, mans{};
+
 	for (std::size_t i = 0; i < in.desks.size(); ++i) {
 		auto& desks = in.desks[i];
 		for (std::size_t j = 0; j < desks.size(); ++j) {
@@ -16,8 +17,9 @@ void solver(const Input& in, Output& o) {
 				case Desk::developer:
 					if (dev < in.developers.size()) {
 						o.developers[dev++] = Pos{j, i};
-					}
+					}	
 					break;
+				default: break;
 			}
 		}
 	}
